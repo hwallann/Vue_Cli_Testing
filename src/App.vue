b98c9866fbcaaf96ec62cdcd1d226fcae469ad33
hwallann <hwallann@gmail.com>
2018-09-15 20:59:10 +0200

    Practical usage of slots in making templates

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
     <form-helper>
-      <!--<h2 slot="title">I am the slot title</h2>-->
-      <h2 slot="title">{{ title }}</h2>
-      <p slot="text">I am the paragraph text for the slot</p>
+      <div slot="form-header">
+        <h3>This is the title of the form</h3>
+        <p>Information about the form</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required="">
+        <input type="password" placeholder="password" required="">
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Submit</button>
+      </div>
     </form-helper>
   </div>
 </template>
@@ -21,6 +29,7 @@ export default {
     }
   },
   methods: {
+
 
   }
 }
