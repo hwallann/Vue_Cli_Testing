f30148e301528911d5485eabe82618e722749f32
hwallann <hwallann@gmail.com>
2018-09-15 19:39:47 +0200

    Done Lesson 23

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -6,6 +6,7 @@
         <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
       </li>
     </ul>
+    <button v-on:click="deleteNinja">Delete ninja</button>
   </div>
 </template>
 
@@ -25,7 +26,12 @@
       return {
 
       }
-}
+    },
+    methods: {
+      deleteNinja: function() {
+        this.ninjas.pop()
+      }
+    }
   }
 </script>
 
