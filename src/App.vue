e154d2d2c1f1fa99319f458dbb1a3e8e038a7816
hwallann <hwallann@gmail.com>
2018-09-15 20:48:33 +0200

    Easy use tutorial of slots

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
-    <p>{{ greeting() }}</p>
+    <form-helper>
+      <!--<h2 slot="title">I am the slot title</h2>-->
+      <h2 slot="title">{{ title }}</h2>
+      <p slot="text">I am the paragraph text for the slot</p>
+    </form-helper>
   </div>
 </template>
 
 <script>
+// Imports
+import formHelper from './components/formHelper.vue'
 export default {
+  components: {
+    'form-helper': formHelper
+  },
   data () {
     return {
-      title: 'Your first Vue file, wooo!'
+      title: 'I am a dynamic slot title'
     }
   },
-
   methods: {
-    greeting: function(){
-      return 'heeeey cowboy!';
-    }
+
   }
 }
 </script>
