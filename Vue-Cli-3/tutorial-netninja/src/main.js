190c174c1d77916e6c89fc75a5e5c1688d4e3203
hwa018 <hwa018@uit.no>
2018-09-20 16:13:18 +0200

    Add rainbow directive to project. Start on 'theme' directive

--- Vue-Cli-3/tutorial-netninja/src/main.js
+++ Vue-Cli-3/tutorial-netninja/src/main.js
@@ -4,10 +4,24 @@ import router from './router'
 import store from './store'
 import VueResource from 'vue-resource'
 
+Vue.config.productionTip = false
 
 Vue.use(VueResource)
 
-Vue.config.productionTip = false
+// Custom directives
+Vue.directive('rainbow', {
+	bind(el, binding, vnode){
+		el.style.color = "#" + Math.random().toString().slice(2,8);
+	}
+});
+
+Vue.directive('theme' {
+	bind(el, binding, vnode) {
+		if(binding.value == 'wide') {
+			el.style.maxWidth = "1200px";
+		}
+	}
+})
 
 new Vue({
   router,
