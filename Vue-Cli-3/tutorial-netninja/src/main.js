3b7ac7105c3f95ddfee06abdafec5f7a4b7f2c08
hwa018 <hwa018@uit.no>
2018-09-21 09:34:08 +0200

    Add custom directive custom arguments

--- Vue-Cli-3/tutorial-netninja/src/main.js
+++ Vue-Cli-3/tutorial-netninja/src/main.js
@@ -22,6 +22,11 @@ Vue.directive('theme', {
 		} else if (binding.value == 'narrow') {
 			el.style.maxWidth = "650px";
 		}
+
+		if (binding.arg == 'column') {
+			el.style.background = '#ddd';
+			el.style.padding = '20px';
+		}
 	}
 })
 
