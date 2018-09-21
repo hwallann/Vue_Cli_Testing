1503725dc3b76d66d36af2455e28efe8f8f8ba53
hwa018 <hwa018@uit.no>
2018-09-21 10:05:39 +0200

    Store directive locally instead of gloabally

--- Vue-Cli-3/tutorial-netninja/src/main.js
+++ Vue-Cli-3/tutorial-netninja/src/main.js
@@ -9,11 +9,13 @@ Vue.config.productionTip = false
 Vue.use(VueResource)
 
 // Custom directives
+/*
 Vue.directive('rainbow', {
 	bind(el, binding, vnode){
 		el.style.color = "#" + Math.random().toString().slice(2,8);
 	}
 });
+*/
 
 Vue.directive('theme', {
 	bind(el, binding, vnode) {
