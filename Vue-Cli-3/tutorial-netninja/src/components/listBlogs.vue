ca2ef9fd29b0acaae2c0d52a27c97f9b4a949070
hwa018 <hwa018@uit.no>
2018-09-21 12:14:51 +0200

    Done Lesson 38: Mixins. Externalize popular pieces of code for reusage

--- Vue-Cli-3/tutorial-netninja/src/components/listBlogs.vue
+++ Vue-Cli-3/tutorial-netninja/src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
-  <div v-theme:column="'narrow'" id="show-blogs">
-    <h1>All blog Articles</h1>
+  <div id="show-blogs">
+    <h1>List Blog Titles</h1>
     <input type="text" v-model="search" placeholder="search blogs">
     <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
-      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
